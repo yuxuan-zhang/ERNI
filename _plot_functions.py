83e913120d8bc5618cb6420453bfc0531c16c7cb
Yuxuan Zhang <zhangy6x@gmail.com>
2017-07-26 11:57:05 -0400

    Updated and removed repeated parameters.

--- _plot_functions.py
+++ _plot_functions.py
@@ -169,26 +169,27 @@ def modify_density_dict_by_enrichment(density_gcm3_dict, enriched_element, isoto
 
 def get_tot_trans_for_single_ele(_input_ele_str, _input_thick_mm, energy_max, energy_min, energy_sub):
     # Input sample name or names as str, case sensitive
-    # _input_ele_str = 'CoAg'  # input('Please input the chemicals? ')
+    # _input_formula = 'AgCo'  # input('Please input the chemicals? ')
     # _input_thick_mm = 0.025  # float(input('Please input the thickness or majority thickness of stacked foils in mm : '))
-    _database = 'ENDF_VIII'
     _input_thick_cm = _input_thick_mm / 10
+    _database = 'ENDF_VIII'
     # energy_max = 300  # max incident energy in eV
     # energy_min = 0  # min incident energy in eV
-    # energy_sub = 100
-    sub_x = energy_sub * (energy_max - energy_min)  # subdivided new x-axis
+    # energy_sub = 100  # steps used to interpolate database
+    sub_x = energy_sub * (energy_max - energy_min)  # steps used to interpolate database
+    # compound_boo = 'N'  # Compound or single/multi elements foil/stacked foils: Y/N?
 
-    # '''Describe your sample: '''
-    # stacked_foil_boo = 'Y'  # Single element foil or stacked foils: Y/N?
+    # '''Input for dict modification in certain cases: '''
     # # Thickness input:
     # special_thick_boo = 'N'
     # special_thick_element_str = str
     # special_thick_mm_list = []
     # special_thick_cm_list = np.array(special_thick_mm_list) / 10
     # # Enriched isotope ratio input:
-    # enrichment_boo = 'N'  # Isotopic enreiched or depleted: Y/N?
-    # enriched_element_str = str
-    # input_ratio_dict = {}
+    # enrichment_boo = 'N'  # Isotopic enriched or depleted: Y/N?
+    # enriched_element_str = 'U'
+    # input_ratio_dict = {'U': [0., 0., .15, .85]}
+    # # 'O': [1., 0., 0.]}  #{'233-U': 0., '234-U': 0., '235-U': 0.15, '238-U': 0.85}}
     # # Special density input:
     # special_density_boo = 'N'
     # special_density_element_str = str
@@ -223,7 +224,7 @@ def get_tot_trans_for_single_ele(_input_ele_str, _input_thick_mm, energy_max, en
 
     # DICT 1: Thickness dict with option for modification
     thick_cm_dict = _functions.repeat_value_dict(elements, _input_thick_cm)
-    # if stacked_foil_boo == 'Y':
+    # if compound_boo == 'N':
     #     if special_thick_boo == 'Y':
     #         thick_cm_dict = _plot_functions.modify_thick_cm_dict_by_input(thick_cm_dict, special_thick_element_str,
     #                                                                       special_thick_cm_list)
@@ -254,8 +255,8 @@ def get_tot_trans_for_single_ele(_input_ele_str, _input_thick_mm, energy_max, en
     #     density_gcm3_dict = _plot_functions.modify_density_dict_by_enrichment(density_gcm3_dict, enriched_element,
     #                                                                           isotope_dict, iso_ratio_dicts)
     #
-    # # Update DICT 5: Density dict, if special case considered
-    # if stacked_foil_boo == 'Y':
+    # # Update DICT 5: Density dict, if special case encountered
+    # if compound_boo == 'N':
     #     if special_density_boo == 'Y':
     #         # Stacked foils and would like to modify density for specific element
     #         density_gcm3_dict = _plot_functions.modify_density_dict_by_input(density_gcm3_dict,
@@ -268,38 +269,29 @@ def get_tot_trans_for_single_ele(_input_ele_str, _input_thick_mm, energy_max, en
 
     print('Thickness (cm): ', thick_cm_dict)
     print('Density (g/cm^3): ', density_gcm3_dict)
-    print('Isotopic at.%', iso_ratio_dicts)
-    print('Isotopic mass: ', iso_mass_dicts)
+    print('Isotopic ratio (at.%)', iso_ratio_dicts)
+    print('Molar weight (g/mol): ', molar_mass_dict)
 
     '''For plotting the database'''
-    avo_divi_mass_iso_ele_dict = {}  # For number of atoms per cm3 calculation
     sigma_iso_ele_eleisodict = {}  # For transmission calculation at isotope level
     sigma_iso_ele_sum_eledict = {}  # For transmission calculation at element level
     sigma_iso_ele_sum_l_eledict = {}
     sigma_iso_ele_l_eleisodict = {}
     df_raw_dict = {}  # Raw sigma data for elements and isotopes
-    atoms_per_cm3_dict = {}
 
-    for ele in elements:
-        # isotopes_list = list(dict.keys(iso_ratio_dicts[ele]))
-        iso_ratio_list = list(dict.values(iso_ratio_dicts[ele]))
+    for el in elements:
+        # isotopes_list = list(dict.keys(iso_ratio_dicts[el]))
+        iso_ratio_list = list(dict.values(iso_ratio_dicts[el]))
         iso_ratio_array = np.array(iso_ratio_list)
-        iso_mass_list = list(dict.values(iso_mass_dicts[ele]))
+        iso_mass_list = list(dict.values(iso_mass_dicts[el]))
         iso_mass_array = np.array(iso_mass_list)
-        ele_at_ratio = formula_dict[ele] / sum_ratios
-
-        # A part for getting atoms_per_cm3, this part is irrelevant to fitting parameters, and will be exported for fitting
-        avo_divi_mass_iso_ele_dict[ele] = avogadro_number / (sum(iso_ratio_array * iso_mass_array) * ele_at_ratio)
-        # if stacked_foil_boo == 'Y':
-            # Multiple foils stacked
-            # sample_density_dict[ele] = density_gcm3_dict[ele] * 1
-        atoms_per_cm3_dict[ele] = density_gcm3_dict[ele] * avo_divi_mass_iso_ele_dict[ele]
+        ele_at_ratio = formula_dict[el] / sum_ratios
 
         # Get sigma related terms
-        file_names = _functions.get_file_path(_database, ele)
-        x_energy, sigma_iso_ele_isodict, sigma_iso_ele_l_isodict, sigma_iso_ele_sum, df_raw_dict[ele] \
-            = get_xy_from_database(iso_ratio_dicts[ele],
-                                   thick_cm_dict[ele],
+        file_names = _functions.get_file_path(_database, el)
+        x_energy, sigma_iso_ele_isodict, sigma_iso_ele_l_isodict, sigma_iso_ele_sum, df_raw_dict[el] \
+            = get_xy_from_database(iso_ratio_dicts[el],
+                                   thick_cm_dict[el],
                                    file_names,
                                    energy_min,
                                    energy_max,
@@ -307,32 +299,42 @@ def get_tot_trans_for_single_ele(_input_ele_str, _input_thick_mm, energy_max, en
                                    sub_x,
                                    ele_at_ratio)
         # Two level dict of isotopic array of (L * sigma * iso_ratio * ele_ratio)
-        sigma_iso_ele_l_eleisodict[ele] = sigma_iso_ele_l_isodict
+        sigma_iso_ele_l_eleisodict[el] = sigma_iso_ele_l_isodict
         # One level dict of elemental array of (L * sigma * iso_ratio * ele_ratio)
-        sigma_iso_ele_sum_l_eledict[ele] = sigma_iso_ele_sum * thick_cm_dict[ele]
+        sigma_iso_ele_sum_l_eledict[el] = sigma_iso_ele_sum * thick_cm_dict[el]
 
         # Two level dict of isotopic array of (sigma * iso_ratio * ele_ratio)
-        sigma_iso_ele_eleisodict[ele] = sigma_iso_ele_isodict
+        sigma_iso_ele_eleisodict[el] = sigma_iso_ele_isodict
         # One level dict of elemental array of (sigma * iso_ratio * ele_ratio)
-        sigma_iso_ele_sum_eledict[ele] = sigma_iso_ele_sum
+        sigma_iso_ele_sum_eledict[el] = sigma_iso_ele_sum
 
-    # if stacked_foil_boo == 'Y':
+    # Get Thickness * number of atoms per cm^3
+    # if compound_boo == 'N':
         # Stacked foils or single foil
-    mixed_l_n_avo = l_x_n_multi_ele_stack(elements, thick_cm_dict, density_gcm3_dict, molar_mass_dict)
+    mixed_l_n_avo = l_x_n_multi_ele_stack(elements,
+                                          thick_cm_dict,
+                                          density_gcm3_dict,
+                                          molar_mass_dict)
     # else:
+    #     # For compound
     #     thick_cm_list = list(dict.values(thick_cm_dict))
     #     thick_cm = thick_cm_list[0]
-    #     sample_density = density_gcm3_dict[ele]
-    #     avo_divi_mass_iso_ele_list = list(dict.values(avo_divi_mass_iso_ele_dict))
-        # avo_divi_mass_iso_ele_sum = sum(np.array(avo_divi_mass_iso_ele_list))
-        # Get atoms per cm3 for mixture
-        # mixed_atoms_per_cm3 = sample_density * avo_divi_mass_iso_ele_sum
-        # mixed_l_n_avo = thick_cm * mixed_atoms_per_cm3
-    # sum of (sigma * ele_ratio * iso_ratio * l)
+    #     compound_density = input_tot_density
+    #     mixed_l_n_avo = _plot_functions.l_x_n_compound(elements,
+    #                                                    thick_cm,
+    #                                                    compound_density,
+    #                                                    molar_mass_dict,
+    #                                                    formula_dict,
+    #                                                    sum_ratios)
+
+    # Get the tot transmission for all
     # yi_values_l = list(dict.values(sigma_iso_ele_sum_l_eledict))
     # yi_values_l_sum = sum(yi_values_l)
-    # sum of (sigma * ele_ratio * iso_ratio)
+    # # sum of (sigma * ele_ratio * iso_ratio * l)
     yi_values = list(dict.values(sigma_iso_ele_sum_eledict))
     yi_values_sum = sum(yi_values)
+    # sum of (sigma * ele_ratio * iso_ratio)
+    # print(yi_values)
     y_trans_tot = _functions.sig_l_2trans_quick(mixed_l_n_avo, yi_values_sum)
+
     return x_energy, y_trans_tot
