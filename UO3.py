e30d51ac59f2266b0c58afc606d70bbbab6bf772
Yuxuan Zhang <zhangy6x@gmail.com>
2017-08-15 16:24:31 -0400

    updated calibration with ImagingReso

--- UO3.py
+++ UO3.py
@@ -7,16 +7,17 @@ _energy_min = 1e-5
 _energy_max = 300
 _energy_step = 0.01
 # Input sample name or names as str, case sensitive
-_layer_1 = 'UO2'
-_thickness_1 = 1 # mm
-_density_1 = 10.97 * 0.9 # g/cm3 deviated due to porosity
+_layer_1 = 'U3O8'
+_thickness_1 = 0.5 # mm
+_density_1 = 2 # g/cm3 deviated due to porosity
 
-# _layer_2 = 'Ag'
-# _thickness_2 = 0.025 # mm
+_layer_2 = 'Gd'
+_thickness_2 = 0.075 # mm
 
 o_reso = Resonance(energy_min=_energy_min, energy_max=_energy_max, energy_step=_energy_step)
-o_reso.add_layer(formula=_layer_1, thickness=_thickness_1)
-# o_reso.add_layer(formula=_layer_2, thickness=_thickness_2)
-
+o_reso.add_layer(formula=_layer_1, thickness=_thickness_1, density=_density_1)
+o_reso.add_layer(formula=_layer_2, thickness=_thickness_2)
+# new_list_ratio = [0., 0., 0.15, 0.85]
+# o_reso.set_isotopic_ratio(compound='UO3', element='U', list_ratio=new_list_ratio)
 # pprint.pprint(o_reso)
 o_reso.plot(mixed=True, all_layers=True, transmission=False)
--- layers.py
+++ layers.py
@@ -7,7 +7,7 @@ _energy_min = 1e-5
 _energy_max = 800
 _energy_step = 0.01
 # Input sample name or names as str, case sensitive
-_layer_1 = 'I'
+_layer_1 = 'IAg'
 _thickness_1 = 0.025 # mm
 # _density_1 = 8 # g/cm3 deviated due to porosity
 
@@ -16,7 +16,7 @@ _thickness_2 = 0.025 # mm
 
 o_reso = Resonance(energy_min=_energy_min, energy_max=_energy_max, energy_step=_energy_step)
 o_reso.add_layer(formula=_layer_1, thickness=_thickness_1)
-o_reso.add_layer(formula=_layer_2, thickness=_thickness_2)
+# o_reso.add_layer(formula=_layer_2, thickness=_thickness_2)
 
 pprint.pprint(o_reso)
-# o_reso.plot(mixed=True, all_layers=True, transmission=False)
+o_reso.plot(mixed=True, all_elements=True, transmission=False)
